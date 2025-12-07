39188f4f1d53caa80a7655f92fd7e6c9024137b3
Jon <due@wxwhatever.com>
2025-12-07 21:12:32 +1000

    Update for Lootr .96.
    RandomizableContainerBlockEntity -> BlockEntity (so we check for
    `RandomizableContainer` itself instead), and the seed is provided -- we
    don't need to fetch it manually.
    
    Finally, use the correct processor.

--- common/src/main/java/it/hurts/shatterbyte/clavis/common/compat/lootr/LockPostProcessor.java
+++ common/src/main/java/it/hurts/shatterbyte/clavis/common/compat/lootr/LockPostProcessor.java
@@ -8,24 +8,33 @@ import it.hurts.shatterbyte.clavis.common.data.LootUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
+import net.minecraft.world.RandomizableContainer;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootTable;
-import noobanidus.mods.lootr.common.api.processor.ILootrPostProcessor;
+import noobanidus.mods.lootr.common.api.processor.ILootrBlockEntityProcessor;
 
 import java.util.UUID;
 
-public class LockPostProcessor implements ILootrPostProcessor {
+public class LockPostProcessor implements ILootrBlockEntityProcessor.Post {
   @Override
-  public void process(ServerLevel serverLevel, BlockPos entityPos, RandomizableContainerBlockEntity rbe, BlockState blockState, ResourceKey<LootTable> resourceKey) {
+  public void process(ServerLevel serverLevel, BlockPos entityPos, BlockEntity rbe, BlockState blockState, ResourceKey<LootTable> resourceKey, long seed) {
+    if (resourceKey == null) {
+      return;
+    }
+
+    if (!(rbe instanceof RandomizableContainer container)) {
+      return;
+    }
+
     LockManager.getLocksAt(serverLevel, null, entityPos).forEach(lock -> LockManager.removeLock(serverLevel, lock));
 
-    float difficulty = (float) LootUtils.calculateDifficulty(serverLevel, entityPos, rbe, 20, false, null);
+    float difficulty = (float) LootUtils.calculateDifficulty(serverLevel, entityPos, container, 20, false, null);
 
     if (difficulty < Clavis.CONFIG.getDifficultyThreshold()) {
       return;
     }
 
-    LockManager.addLock(serverLevel, new Lock(UUID.randomUUID(), new Box(entityPos), difficulty, rbe.getLootTableSeed(), true));
+    LockManager.addLock(serverLevel, new Lock(UUID.randomUUID(), new Box(entityPos), difficulty, seed, true));
   }
 }
